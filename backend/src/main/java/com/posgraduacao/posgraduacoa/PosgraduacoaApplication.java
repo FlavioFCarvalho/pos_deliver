7d4d6cccd43cea77d9308280f5b12b490d1293b3
FlavioFCarvalho <flaviocfr@gmail.com>
2023-07-18 21:04:54 -0300

    first commit

---
+++ backend/src/main/java/com/posgraduacao/posgraduacoa/PosgraduacoaApplication.java
@@ -0,0 +1,13 @@
+package com.posgraduacao.posgraduacoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosgraduacoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosgraduacoaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/posgraduacao/posgraduacoa/PosgraduacoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posgraduacao.posgraduacoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosgraduacoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
